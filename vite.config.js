593ae0ebf20f2e2dc68ac4cdd55428f315445230
Yoshitmani <yoshitmani8@gmail.com>
2026-05-07 12:22:47 +0530

    updated backend url to vercel

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
       "/api": {
-        target: "https://resume-builder-main-backend.onrender.com", // change this port if your server runs on a different port
+        target: "https://event-back-end-snowy.vercel.app",
         changeOrigin: true,
       },
     },
